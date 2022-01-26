187069751abb05ab9dab971371ccc3e621fcae78
zhouxinseeu <zhouxin@singleronbio.com>
2022-01-27 01:02:19 +0800

    update reagent collection time

--- reagent_notice.php
+++ reagent_notice.php
@@ -3,7 +3,7 @@
 
 <body>
 <?php 
-echo "<ul><li>Reagent collection times: <font color=\"red\">Tues (11:00-12:00)</font> or <font color=\"red\">Fri (11:00-12:00)</font> ONLY. No collection outside these times</li>";
+echo "<ul><li>Reagent collection times: <font color=\"red\">Monday (2:30pm-3:30pm), Wednesday and Friday (11:00am-12:00am)</font>. No collection outside these times</li>";
 echo "<li><font color=\"red\">Please seek your PI's approval before requesting reagents. An email will be sent to your PI.</font></li>";
 echo "<li>Please note the Reagent Request ID (<font color=\"red\">RgRID</font>) for future reference</li>";
 echo "<li>For DNA bioanalyzer, if the number of samples is less than 11, please use the <a href=\"reagent_share.php\"><u style=\"color:red\">DNA shared request</u></a></li>";
