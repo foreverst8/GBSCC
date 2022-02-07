9ef300ab4b266ee01e9b2560959087238bc03785
zhouxinseeu <zhouxin@singleronbio.com>
2022-02-07 22:43:04 +0800

    add test email address

--- email_CC.php
+++ email_CC.php
@@ -1,6 +1,7 @@
 <?PHP
 
 $CC="kaelingtan@umac.mo,tiffanyiu@umac.mo,xinzhou@um.edu.mo,miziq@qq.com,michellecenon@um.edu.mo";
+$test_email="xinzhou@um.edu.mo";
 $main_mesg_email="<br><br><br><br><br>Technical support:<br>Kaeling Tan: kaelingtan@um.edu.mo<br>Tiffany Iu: tiffanyiu@um.edu.mo<br>Michelle Cenon: michellecenon@um.edu.mo <br><br>Bioinformatics support:<br>Xin Zhou: xinzhou@um.edu.mo<br>";
 
 ?>
