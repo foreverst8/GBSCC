c862b63c346ed4a98e85b0013183d1095c93ae04
zhouxinseeu <zhouxin@singleronbio.com>
2022-02-17 21:09:03 +0800

    add freezedrier equipment

---
+++ freezedrier_introduction.php
@@ -0,0 +1,41 @@
+<html>
+
+<head>
+	<style>
+		img {
+			margin-right: 10px;
+		}
+
+		body {
+			margin-left: 20%;
+			margin-right: 20%;
+		}
+	</style>
+
+	<link rel="stylesheet" type="text/css" href="layout/styles/style.css" />
+</head>
+
+<body>
+	<div class="introduction">
+		<h6>Freeze-drier</h6>
+		<br>
+		<p>The freeze dryer technology is a water removal procedure used to preserve perishable materials, with an aim to increase the material’s shelf life and/or prepare them for transport. Freeze drying is the process of freezing the materials, then decreasing the pressure and adding heat to transform frozen water in the material to change into a vapor (sublimate).
+		</p>
+		<br>
+		<br>
+		<img src="./layout/freezedrier.jpg" width="300" height="300" alt="Freeze-drier" title="Freeze-drier">
+		<br>
+		<br>
+		<p><strong>The freeze drier available for users at the core is Labconco FreeZone 4.5 L 105C Benchtop Freeze Dryer.</strong>
+		</p>
+		<br>
+		<br>
+		<p>This instrument is designed for lyophilizing light to moderate sample loads. It’s stainless steel collector coil holds 4.5 liters of ice before defrosting. Free of HCFCs(hydrofluorocarbons)/CFCs(chlorofluorocarbons) and consists of two 1/2 horsepower refrigeration systems that cools the collector to -105° C (-157° F) at 60Hz or -102° C (-152° F) at 50 Hz, fit for low eutectic point samples such as ethanol and acetonitrile.
+		</p>
+		<br>
+		<br>
+		<br>
+	</div>
+</body>
+
+</html>
